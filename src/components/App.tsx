35fb79bffa1bc536b257576364d8cd4174e2b6e1
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-04-09 10:47:10 +0100

    feat: adds simple game board

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,15 @@
+const BOARD_WIDTH = 7;
+const BOARD_HEIGHT = 6;
+const GAME_ARRAY = Array(BOARD_WIDTH * BOARD_HEIGHT).fill(null);
+
 const App = () => {
     return (
         <div className="App">
-            <p>Hello world</p>
+            <div className="board">
+                {GAME_ARRAY.map((value, index) => (
+                    <div key={index} className="board-item"></div>
+                ))}
+            </div>
         </div>
     );
 };
